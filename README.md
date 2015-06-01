f492df94cd3e08e426bcf2baab46df5cf160aa2b
Alex Van Camp <email@alexvan.camp>
2015-06-01 00:32:50 -0500

    Port to axon. Add way more events. Add twitch chat Polymer element.

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 #lfg-siphon
 This is a [NodeCG](http://github.com/nodecg/nodecg) bundle.
 
-This bundle is an interface to [Streen](https://github.com/SupportClass/streen), an IPC wrapper for [twitch-irc](https://github.com/twitch-irc/twitch-irc). 
+This bundle is an interface to [Streen](https://github.com/SupportClass/streen), an IPC wrapper for 
+[twitch-irc](https://github.com/twitch-irc/twitch-irc). 
 Together, Streen and lfg-siphon enable one single twitch-irc instance to power multiple NodeCG instances.
+
+lfg-siphon also provides a custom `twitch-chat` Polymer component which other bundles can easily include in their views
+to get embedded Twitch Chat.
 
 ## Installation
 - Install to `nodecg/bundles/lfg-siphon`
 - Create `nodecg/cfg/lfg-siphon.json` with a list of Twitch chat channels to listen to.
-- By default, lfg-siphon will not expose chat activity, so as to conserve resources. To enable, set `chat` to `true`.
+- To conserve resources, lfg-siphon will not emits events for normal chat messages by default.
+ To enable, set `chat` to `true`.
 
 ### Config Example
 ```json
@@ -17,35 +22,99 @@ Together, Streen and lfg-siphon enable one single twitch-irc instance to power m
 }
 ```
 
+Additionally, there are optional properties `subPort` and `rpcPort`. These default to the same values as Streen.
+
+## methods
+From an extension:
+``` js
+var siphon = nodecg.extensions['lfg-siphon'];
+```
+
+### siphon.timeout(channel, username, seconds)
+Times out a user on a channel for the given number of seconds.
+See the corresponding twitch-irc docs for more info ([link](http://www.twitch-irc.com/docs/twitch-irc/Commands/Timeout)).
+
+## Events
+### siphon.on('connect', function () {})
+When connected to Streen.
+
+### siphon.on('disconnect', function () {})
+Emitted when connected to Streen.
+
+### siphon.on('join', function (channel) {})
+Emitted after joining a chat channel.
+
+### siphon.on('part', function (channel) {})
+Emitted after parting a chat channel.
+
+### siphon.on('chat', function (data) {})
+Emitted on every chat message. `data` has the properties from twitch-irc's `chat` event
+ ([docs](http://www.twitch-irc.com/docs/twitch-irc/Events/Chat)).
+ 
+### siphon.on('timeout', function (data) {})
+Emitted when a user is timed out. `data` has the properties from twitch-irc's `timeout` event
+ ([docs](http://www.twitch-irc.com/docs/twitch-irc/Events/Timeout)).
+ 
+### siphon.on('clearchat', function (channel) {})
+Emitted when a mod clears the chat.
+
+### siphon.on('subscription', function (data) {})
+Emitted on both subscriptions and sub anniversaries (resubs). `data` has the following properties:
+```js
+data.username // Twitch username of the subscriber or resub
+data.channel  // What channel was subscribed to
+data.resub    // Boolean, whether or not this is a resub
+data.months   // If this is a resub, this will be the number of months they have been subscribed for
+data.ts       // Unix timestamp (in milliseconds)
+```
 ## Usage
 ### As a dashboard panel
 If you simply want a list of recent subs on your dashboard, you are done.
 
 ### In other bundles' view pages and dashboard panels
-If you would like to use this data in another bundle, add the following code to your view/panel:
+If you would like to use this data in another bundle, add listen for the desired events in your view/panel.
+
+Example:
 ```javascript
 nodecg.listenFor('subscription', 'lfg-siphon', function(data) {
     console.log(data);
 });
 ```
 
-### In other bundles' extensions
-If you want to use subscription events in another bundle's extension,
-add `lfg-siphon` as a `bundleDependency` in your bundle's [`nodecg.json`](https://github.com/nodecg/nodecg/wiki/nodecg.json)
+To add a Twitch Chat embed, import the custom element and add an instance of it to your HTML:
+```html
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    
+    <!-- Import the custom twitch-chat element from lfg-siphon -->
+    <link rel="import" href="/view/lfg-siphon/twitch-chat.html">
+</head>
+<body>
+    <!-- Add an instance of it to the DOM -->
+    <twitch-chat channel="langeh"></twitch-chat>
+</body>
+</html>
+```
 
-Then add the following code:
+### In other bundles' extensions
+If you want to use chat events in another bundle's extension,
+add `lfg-siphon` as a `bundleDependency` in your bundle's [`nodecg.json`](https://github.com/nodecg/nodecg/wiki/nodecg.json)
+Then listen to the desired events in your extension.
+
+Example:
 ```javascript
 var siphon = nodecg.extensions['lfg-siphon'];
-siphon.on('subscription', function subscription(data) {
-    // Do work.
-    // data.username = Twitch username of subscription
-    // data.channel  = What channel was subscribed to
-    // data.resub    = Boolean, whether or not this is a resub
-    // data.months   = If this is a resub, this will be the number of months they have been subscribed for
-    // data.ts       = Unix timestamp (in milliseconds)
+siphon.on('subscription', function (data) {
+    console.log(data);
 });
 ```
 
-### License
+## Special Thanks
+ - Schmoopiie, whose [twitch-irc](https://github.com/twitch-irc/twitch-irc) library makes this all possible
+ - Night, from whose [OBS Chat](https://nightdev.com/obschat/) embed service the `twitch-chat` element borrows code heavily.
+
+## License
 lfg-siphon is provided under the MIT license, which is available to read in the [LICENSE][] file.
 [license]: LICENSE
--- extension.js
+++ extension.js
@@ -1,60 +1,123 @@
 'use strict';
 
-var os            = require('os');
 var EventEmitter  = require('events').EventEmitter;
-var ipc           = require('node-ipc');
+var axon          = require('axon');
+var rpc           = require('axon-rpc');
+var req           = axon.socket('req');
+var subSock       = axon.socket('sub');
+var rpcClient     = new rpc.Client(req);
 var equal         = require('deep-equal');
-ipc.config.id     = 'siphon';
-ipc.config.retry  = 1500;
-ipc.config.silent = true;
 
 module.exports = function(nodecg) {
     if (!Object.keys(nodecg.bundleConfig).length) {
         throw new Error('[lfg-siphon] No config found in cfg/lfg-siphon.json, aborting!');
     }
 
-    var self = new EventEmitter();
+    var self     = new EventEmitter();
     var channels = nodecg.bundleConfig.channels;
-    var connectFn = os.platform() === 'win32' ? ipc.connectToNet : ipc.connectTo;
-    connectFn('streen', function () {
-        ipc.of.streen.on('connect', function () {
-            nodecg.log.info('Connected to Streen');
-            ipc.of.streen.emit('join', channels);
-        });
+    var SUB_PORT = nodecg.bundleConfig.subPort || 9455;
+    var RPC_PORT = nodecg.bundleConfig.rpcPort || 9456;
 
-        ipc.of.streen.on('disconnect', function () {
-            nodecg.log.warn('Disconnected from Streen');
-        });
+    nodecg.listenFor('getChannels', function(data, cb) {
+        cb(channels);
+    });
 
-        ipc.of.streen.on('joined', function (channel) {
-            nodecg.log.info('Joined channel:', channel);
-        });
+    subSock.connect(SUB_PORT, '127.0.0.1');
+    req.connect(RPC_PORT, '127.0.0.1');
 
-        var lastSub;
-        ipc.of.streen.on('subscription', function (data) {
-            if (channels.indexOf(data.channel) < 0) return;
-            if (equal(lastSub, data)) return;
-            lastSub = data;
-            self.emit('subscription', data);
-            nodecg.sendMessage('subscription', data);
-        });
+    subSock.on('connect', function() {
+        nodecg.log.info('Connected to Streen');
+        channels.forEach(function(channel) {
+            rpcClient.call('join', channel, function(err, alreadyJoined) {
+                if (err) {
+                    nodecg.log.error(err.stack);
+                    return;
+                }
 
-        if (nodecg.bundleConfig.chat) {
-            ipc.of.streen.on('chat', function (data) {
-                if (channels.indexOf(data.channel) < 0) return;
-                self.emit('chat', data.channel, data.user, data.message);
-                nodecg.sendMessage('chat', data);
+                if (alreadyJoined) {
+                    nodecg.log.info('Streen already in channel:', alreadyJoined);
+                }
             });
-        } else {
-            nodecg.log.info('Property "chat" in config is "false", ignoring chat');
+        });
+    });
+
+    subSock.on('disconnect', function() {
+        nodecg.log.warn('Disconnected from Streen');
+        self.emit('disconnect');
+        nodecg.sendMessage('disconnect');
+    });
+
+    var lastSub;
+    subSock.on('message', function(msg) {
+        switch (msg.toString()) {
+            case 'connected':
+                nodecg.log.info('Streen connected to Twitch Chat');
+                break;
+            case 'disconnected':
+                nodecg.log.info('Streen disconnected from Twitch Chat');
+                break;
+            case 'join':
+                var channel = arguments[1];
+                if (channels.indexOf(channel) < 0) return;
+                nodecg.log.info('Joined channel:', channel);
+                self.emit('join', channel);
+                nodecg.sendMessage('join', channel);
+                break;
+            case 'part':
+                var channel = arguments[1];
+                if (channels.indexOf(channel) < 0) return;
+                nodecg.log.warn('Parted from:', channel);
+                self.emit('part', channel);
+                nodecg.sendMessage('part', channel);
+                break;
+            case 'chat':
+                var data = {
+                    channel: arguments[1],
+                    user: arguments[2],
+                    message: arguments[3]
+                };
+                if (channels.indexOf(data.channel) < 0) return;
+                self.emit('chat', data);
+                nodecg.sendMessage('chat', data);
+                break;
+            case 'timeout':
+                var data = {channel: arguments[1], username: arguments[2]};
+                if (channels.indexOf(data.channel) < 0) return;
+                self.emit('timeout', data);
+                nodecg.sendMessage('timeout', data);
+                break;
+            case 'clearchat':
+                var channel = arguments[1];
+                if (channels.indexOf(channel) < 0) return;
+                self.emit('clearchat', channel);
+                nodecg.sendMessage('clearchat', channel);
+                break;
+            case 'subscription':
+                var data = arguments[1];
+                if (channels.indexOf(data.channel) < 0) return;
+                if (equal(lastSub, data)) return;
+                lastSub = data;
+                self.emit('subscription', data);
+                nodecg.sendMessage('subscription', data);
+                break;
         }
     });
 
+    var heartbeatTimeout = setTimeout(heartbeat, 5000);
+    function heartbeat() {
+        rpcClient.call('heartbeat', channels, function(err, interval) {
+            if (err) {
+                nodecg.log.error(err.stack);
+                return;
+            }
+
+            heartbeatTimeout = setTimeout(heartbeat, interval);
+        });
+    }
+
     self.timeout = function(channel, username, seconds) {
-        ipc.of.streen.emit('timeout', {
-            channel: channel,
-            username: username,
-            seconds: seconds
+        rpcClient.call('timeout', channel, username, seconds, function(err){
+            if (err) nodecg.log.error(err.stack);
         });
     };
 
