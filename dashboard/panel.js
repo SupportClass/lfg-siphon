ff84562cfc43b0b1e9bc1e730f2143b364f914a4
cach116 <mcacciatore116@yahoo.com>
2015-07-07 14:42:50 -0400

    Fixed issue where name was not showing in dashboard panel
    data.name is undefined.
    
    data.username is the actual name of the subscriber.

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -2,7 +2,7 @@
 
 nodecg.listenFor('subscription', function (data) {
     var alert = $('.template.sub').clone().removeClass('template').css('display', 'block');
-    alert.find('.name').html(data.name);
+    alert.find('.name').html(data.username);
     if (data.resub) {
         alert.removeClass('alert-info');
         alert.addClass('bg-primary');
